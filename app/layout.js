4f2aca49d1dd4827e6cee29aa1977b3bb85bc31d
Alex Domzalski <alexdomzalski@gmail.com>
2024-10-29 17:57:41 -0400

    trying to fix icons

--- app/layout.js
+++ app/layout.js
@@ -9,6 +9,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Studio Azure",
   description: "Bespoke graphic design, software development, and branding.",
+  icons: {
+    icon: [
+      { rel: 'icon', url: '/favicon.ico' },
+      { rel: 'apple-touch-icon', url: '/apple-icon1.png', sizes: '512x512' },
+      { rel: 'icon', type: 'image/png', url: '/icon1.png', sizes: '192x192' },
+      { rel: 'icon', type: 'image/png', url: '/icon2.png', sizes: '512x512' },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
